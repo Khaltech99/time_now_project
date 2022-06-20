cfd70d613212a5670a9951d639ecf435e5c30568
lawal azeez <olatunjibabatundejunior@gmail.com>
2022-06-19 22:00:19 -0700

    done with initial

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const menu = document.querySelector(".nav-menu");
 const dropdown = document.querySelector(".dropdown");
 const hideit = document.querySelector(".hideit");
+// const navBorder = document.querySelector(".nav-border");
 
 function drop() {
     dropdown.classList.toggle("hideit");
 }
+
 menu.addEventListener("click", drop);
